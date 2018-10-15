bc8bfb6e6aba1fae0052f4ab147cfb732c9761dd
Reazul Hasan Russel <rz.hasan23@gmail.com>
2018-10-15 16:47:18 -0400

    implement RiverSwim MDP, implement PSRL, UCRL2, Bayes UCRL solutions for RiverSwim problem.

---
+++ python_experiments/algorithms.py
@@ -0,0 +1,285 @@
+
+def PSRL(num_states, num_actions, num_next_states, true_transitions, rewards, discount_factor, num_episodes, num_runs, true_solution):
+    """
+    Implements the Posterior Sampling RL algorithm described in Osband2013 ((More) Efficient Reinforcement Learning via Posterior Sampling) paper.
+    
+    Parameters
+    ----------
+    num_states : int
+        Number of states in the MDP
+    num_actions : int
+        Number of actions in the MDP
+    num_next_states : int
+        Number of possible next states. A common formulation is to keep it same as num_states, but with transition probabilities adjusted accordingly.
+    true_transitions : numpy array
+        num_states x num_actions x num_next_states dimensional array containing tru transition parameters.
+    rewards : numpy array
+        num_states dimensional array containing rewards for each state
+    discount_factor : float
+        Discount factor for the MDP
+    num_episodes : int
+        Number of episodes to run
+    num_runs : int
+        Number of runs in each episode, to take average of
+    true_solution : Solution object of CRAAM
+        The solution of the true MDP
+        
+    Returns:
+    --------
+    numpy array
+        Computed regret
+    """
+    # Initialize uniform Dirichlet prior
+    prior = np.ones( (num_states, num_actions, num_next_states) )    
+    samples = np.zeros( (num_states, num_actions, num_next_states) )
+    posterior = prior + samples
+    
+    regret_psrl = np.zeros( (num_episodes, num_runs) )
+    
+    # Run episodes for the PSRL
+    for k in range(num_episodes):
+        for m in range(num_runs):
+            sampled_mdp = crobust.MDP(0, discount_factor)
+            
+            # Compute posterior
+            posterior = posterior+samples
+            
+            for s in range(num_states):
+                for a in range(num_actions):
+                    trp =  np.random.dirichlet(posterior[s,a], 1)[0]
+                    for s_next in range(num_next_states):
+                        sampled_mdp.add_transition(s, a, s_next, trp[s_next], rewards[s_next])
+    
+            # Compute current solution
+            cur_solution = sampled_mdp.solve_mpi()
+            cur_policy = cur_solution.policy
+
+            #action = cur_policy[0] # action for the nonterminal state 0
+            regret_psrl[k,m] = abs(cur_solution[0][0]-true_solution[0][0])
+            #print("PSRL cur_solution[0][0]",cur_solution[0][0], "Regret: ", regret_psrl[k,m])
+            samples = np.zeros((num_states, num_actions, num_next_states))
+            
+            # Follow the policy to collect transition samples
+            cur_state = 0
+            for h in range(horizon):
+                action = cur_policy[cur_state]
+                #print("cur_state", cur_state, "cur_action", action)
+                next_state = np.random.choice(num_next_states, 1, p=true_transitions[cur_state, action])[0]
+                #print("next_state", next_state)
+                samples[cur_state, action, next_state] += 1
+                cur_state = next_state
+                
+    #regret_psrl = np.mean(regret_psrl, axis=1)
+    return np.mean(regret_psrl, axis=1)
+    
+def compute_bayesian_threshold(points, nominal_point, confidence_level):
+    """
+    Computes an empirical L1 threshold from samples from a posterior distribution
+    
+    Parameters
+    ----------
+    points : numpy array
+        Array of posterior points
+    nominal_point : numpy array
+        Array containing nominal/average transition probabilities
+    confidence_level : float
+        Required confidence level
+    """
+    distances = [np.linalg.norm(p - nominal_point, ord = 1) for p in points]
+    confidence_rank = math.ceil(len(points) * confidence_level)
+    threshold = np.partition(distances, confidence_rank)[confidence_rank]
+    return threshold
+    
+def BayesUCRL(num_states, num_actions, num_next_states, true_transitions, rewards, discount_factor, confidence, num_bayes_samples, num_episodes, num_runs, true_solution):
+    """
+    Implements the Bayes UCRL idea. Computes ambiguity set from posterior samples for required confidence levels.
+    
+    Parameters
+    ----------
+    num_states : int
+        Number of states in the MDP
+    num_actions : int
+        Number of actions in the MDP
+    num_next_states : int
+        Number of possible next states. A common formulation is to keep it same as num_states, but with transition probabilities adjusted accordingly.
+    true_transitions : numpy array
+        num_states x num_actions x num_next_states dimensional array containing tru transition parameters.
+    rewards : numpy array
+        num_states dimensional array containing rewards for each state
+    discount_factor : float
+        Discount factor for the MDP
+    confidence : float
+        The required PAC confidence
+    num_bayes_samples : int
+        Number of Bayes samples to be taken from posterior
+    num_episodes : int
+        Number of episodes to run
+    num_runs : int
+        Number of runs in each episode, to take average of
+    true_solution : Solution object of CRAAM
+        The solution of the true MDP
+        
+    Returns:
+    --------
+    numpy array
+        Computed regret
+    """  
+    #num_bayes_samples = 20
+    # Initialize uniform Dirichlet prior
+    prior = np.ones( (num_states, num_actions, num_next_states) )    
+    samples = np.zeros((num_states, num_actions, num_next_states))
+    posterior = prior + samples
+    
+    regret_bayes_ucrl = np.zeros( (num_episodes, num_runs) )
+    
+    # Run episodes for the PSRL
+    for k in range(num_episodes):
+        for m in range(num_runs):
+            sampled_mdp = crobust.MDP(0, discount_factor)
+            
+            # Compute posterior
+            posterior = posterior+samples
+            thresholds = [[] for _ in range(3)]
+            
+            for s in range(num_states):
+                for a in range(num_actions):
+                    bayes_samples =  np.random.dirichlet(posterior[s,a], num_bayes_samples)
+                    nominal_point_bayes = np.mean(bayes_samples, axis=0)
+                    nominal_point_bayes /= np.sum(nominal_point_bayes)
+                    
+                    bayes_threshold = compute_bayesian_threshold(bayes_samples, nominal_point_bayes, confidence)
+                    
+                    for s_next in range(num_next_states):
+                        sampled_mdp.add_transition(s, a, s_next, nominal_point_bayes[s_next], rewards[s_next])
+                        
+                    # construct the threshold for each state-action
+                    thresholds[0].append(s) # from state
+                    thresholds[1].append(a) # action
+                    thresholds[2].append(bayes_threshold) # allowed deviation
+            
+            # Compute current solution
+            cur_solution = sampled_mdp.rsolve_mpi(b"optimistic_l1",np.array(thresholds))
+            cur_policy = cur_solution.policy
+            #action = cur_policy[0] # action for the nonterminal state 0
+            regret_bayes_ucrl[k,m] = abs(abs(cur_solution[0][0])-true_solution[0][0])
+            #print("Bayes UCRL cur_solution[0][0]",cur_solution[0][0], "Regret: ", regret_bayes_ucrl[k,m])
+            samples = np.zeros((num_states, num_actions, num_next_states))
+                
+            # Follow the policy to collect transition samples
+            cur_state = 0
+            for h in range(horizon):
+                action = cur_policy[cur_state]
+                #print("cur_state", cur_state, "cur_action", action)
+                next_state = np.random.choice(num_next_states, 1, p=true_transitions[cur_state, action])[0]
+                #print("next_state", next_state)
+                samples[cur_state, action, next_state] += 1
+                cur_state = next_state
+
+    #regret_bayes_ucrl = np.mean(regret_bayes_ucrl, axis=1)
+    return np.mean(regret_bayes_ucrl, axis=1)
+    
+    
+def UCRL2(num_states, num_actions, num_next_states, true_transitions, rewards, discount_factor, num_episodes, num_runs, true_solution):
+    """
+    Implements the UCRL2 algorithm described in Jaksch2010 (Near-optimal Regret Bounds for Reinforcement Learning) paper.
+    
+    Parameters
+    ----------
+    num_states : int
+        Number of states in the MDP
+    num_actions : int
+        Number of actions in the MDP
+    num_next_states : int
+        Number of possible next states. A common formulation is to keep it same as num_states, but with transition probabilities adjusted accordingly.
+    true_transitions : numpy array
+        num_states x num_actions x num_next_states dimensional array containing tru transition parameters.
+    rewards : numpy array
+        num_states dimensional array containing rewards for each state
+    discount_factor : float
+        Discount factor for the MDP
+    num_episodes : int
+        Number of episodes to run
+    num_runs : int
+        Number of runs in each episode, to take average of
+    true_solution : Solution object of CRAAM
+        The solution of the true MDP
+        
+    Returns:
+    --------
+    numpy array
+        Computed regret
+    """
+    # ***start UCRL2***
+    #num_next_states = num_states
+    regret_ucrl = np.zeros( (num_runs, num_episodes) )
+    
+    #num_runs, num_episodes = 1, 1
+    
+    for m in range(num_runs):
+        t=1
+        # state-action counts
+        Nk = np.zeros( (num_states, num_actions) )
+        Nk_ = np.zeros( (num_states, num_actions) )
+        
+        # accumulated rewards
+        Rk = np.zeros( (num_states, num_actions) )
+        
+        # accumulated transition counts, initialized to uniform transition
+        Pk = np.ones( (num_states, num_actions, num_next_states) )/num_next_states
+    
+        for k in range(num_episodes):
+
+            # ***Initialize
+            tk = t #set the start time of episode k
+            Vk = np.zeros( (num_states, num_actions) ) # init the state-action count for episode k
+            Nk += Nk_
+            r_hat = [ Rk[s,a]/max(1,Nk[s,a]) for s in range(num_states) for a in range(num_actions)]
+            p_hat = np.array([ Pk[s,a,s_next]/max(1,Nk[s,a]) for s in range(num_states) for a in range(num_actions)\
+                                                                for s_next in range(num_next_states) ]).reshape((num_states, num_actions,num_next_states))
+            for s in range(num_states):
+                for a in range(num_actions):
+                    p_hat[s,a] /= np.sum(p_hat[s,a])
+                    
+            # ***Compute policy
+            psi_r = [math.sqrt(7*math.log(2*num_next_states*num_actions*tk/confidence)/(2*max(1,Nk[s,a]))) for s in range(num_states) for a in range(num_actions)]
+            psi_a = [math.sqrt(14*num_next_states*math.log(2*num_actions*tk/confidence)/(max(1,Nk[s,a]))) for s in range(num_states) for a in range(num_actions)]
+            
+            estimated_mdp = crobust.MDP(0, discount_factor)
+            thresholds = [[] for _ in range(3)]
+            
+            for s in range(num_states):
+                for a in range(num_actions):
+                    
+                    for s_next in range(num_next_states):
+                        estimated_mdp.add_transition(s, a, s_next, p_hat[s,a,s_next], rewards[s_next]) # as the reward is upper bounded by psi_r from mean reward r_hat
+                    # construct the threshold for each state-action
+                    thresholds[0].append(s) # from state
+                    thresholds[1].append(a) # action
+                    thresholds[2].append(psi_a[a]) # allowed deviation
+            #print(estimated_mdp.to_json())
+
+            computed_solution = estimated_mdp.rsolve_mpi(b"optimistic_l1",np.array(thresholds))
+            computed_policy = computed_solution.policy
+
+            regret_ucrl[m,k] = abs(abs(computed_solution[0][0])-true_solution[0][0])
+        
+            # ***Execute policy
+            Nk_ = np.zeros( (num_states, num_actions) )
+            cur_state = 0
+
+            for h in range(horizon): #Vk[action] < max(1,Nk[action]):
+                action = computed_policy[cur_state]
+                next_state = np.random.choice(num_next_states, 1, p=true_transitions[cur_state, action])[0]
+                reward = rewards[next_state]
+                Vk[cur_state, action] += 1
+                t += 1
+                
+                Rk[cur_state, action] += 1
+                Pk[cur_state, action, next_state] += 1
+                Nk_[cur_state, action] += 1
+                cur_state = next_state
+
+    #regret_ucrl = np.mean(regret_ucrl, axis=0)
+    return np.mean(regret_ucrl, axis=0)
+    
+    
--- python_experiments/simple_experiments.py
+++ python_experiments/simple_experiments.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 #import tqdm
 import pickle
 import datetime
+import itertools
     
 ### The problem setup is simple: we've 1 non terminal state, indexed as 0. From state 0, we've 3 possible actions, each of which can lead to 3 next terminal states.
 if __name__ == "__main__":
@@ -15,8 +16,8 @@ if __name__ == "__main__":
     num_next_states = 3
     num_actions = 3
     discount_factor = 0.9
-    num_episodes = 20
-    num_runs = 10
+    num_episodes = 50
+    num_runs = 20
     horizon = 5
     
     # rewards for 3 possible next terminal states
@@ -31,9 +32,10 @@ if __name__ == "__main__":
         for s in range(num_next_states):
             true_mdp.add_transition(0, a, s+1, transitions[a, s], rewards[s])
     true_solution = true_mdp.solve_mpi()
-
+    print(true_solution)
+    
 ### UCRL2
-
+if __name__ == "__main__":
     # ***start UCRL2***
     t=1
     regret_ucrl = np.zeros( (num_runs, num_episodes) )
@@ -59,17 +61,20 @@ if __name__ == "__main__":
             r_hat = [ Rk[a]/max(1,Nk[a]) for a in range(num_actions)]
             p_hat = np.array([ Pk[a,s]/max(1,Nk[a]) for a in range(num_actions) for s in range(num_next_states) ]).reshape((num_actions,num_next_states))
             
-            
             # ***Compute policy
             psi_r = [math.sqrt(7*math.log(2*num_next_states*num_actions*tk/confidence)/(2*max(1,Nk[a]))) for a in range(num_actions)]
             psi_a = [math.sqrt(14*num_next_states*math.log(2*num_actions*tk/confidence)/(max(1,Nk[a]))) for a in range(num_actions)]
             
             estimated_mdp = crobust.MDP(0, discount_factor)
             thresholds = [[] for _ in range(3)]
-        
+            
             for a in range(num_actions):
+                
+                #transition_prob = crobust.worstcase_l1_dst(-rewards, p_hat[a], psi_a[a])[0]
+                
                 for s in range(num_next_states):
-                    estimated_mdp.add_transition(0, a, s+1, p_hat[a,s], -rewards[a])#r_hat[a]+psi_r[a]) # as the reward is upper bounded by psi_r from mean reward r_hat
+                    
+                    estimated_mdp.add_transition(0, a, s+1, p_hat[a,s], rewards[a])#r_hat[a]+psi_r[a]) # as the reward is upper bounded by psi_r from mean reward r_hat
                     
                     # construct the threshold for each state-action
                     thresholds[0].append(0) # from state
@@ -77,16 +82,15 @@ if __name__ == "__main__":
                     thresholds[2].append(psi_a[a]) # allowed deviation
             #print(estimated_mdp.to_json())
         
-            computed_solution = estimated_mdp.rsolve_mpi(b"robust_l1",np.array(thresholds))
+            computed_solution = estimated_mdp.rsolve_mpi(b"optimistic_l1",np.array(thresholds)) # solve_mpi()
             computed_policy = computed_solution.policy
-            #print("episode", k , "computed_solution", computed_solution, "computed_policy", computed_policy)
-            
-            regret_ucrl[m,k] = abs(-computed_solution[0][0]-true_solution[0][0])
-            
+            #print("computed_solution", computed_solution[0][0], "true_solution[0][0]", true_solution[0][0])
+            regret_ucrl[m,k] = abs(abs(computed_solution[0][0])-true_solution[0][0])
+            print("UCRL cur_solution",computed_solution, "Regret: ", regret_ucrl[m,k])
             # ***Execute policy
             Nk_ = np.zeros(num_actions)
             action = computed_policy[0] # action for the nonterminal state 0
-            while Vk[action] < max(1,Nk[action]):
+            for _ in range(horizon): #Vk[action] < max(1,Nk[action]):
                 next_state = np.random.choice(num_next_states, 1, p=transitions[action])
                 reward = rewards[next_state]
                 Vk[action] += 1
@@ -105,7 +109,6 @@ if __name__ == "__main__":
     
     
 ### Posterior Sampling Reinforcement Learning (PSRL)
-
 if __name__ == "__main__":    
     # Initialize uniform Dirichlet prior
     prior = [np.ones(num_next_states) for _ in range(num_actions)]
@@ -125,14 +128,14 @@ if __name__ == "__main__":
             for a in range(num_actions):
                 trp =  np.random.dirichlet(posterior[a], 1)[0]
                 for s in range(num_next_states):
-                    sampled_mdp.add_transition(0, a, s+1, trp[s], -rewards[s])
+                    sampled_mdp.add_transition(0, a, s+1, trp[s], rewards[s])
             
             # Compute current solution
             cur_solution = sampled_mdp.solve_mpi()
             cur_policy = cur_solution.policy
             action = cur_policy[0] # action for the nonterminal state 0
-            regret_psrl[k,m] = abs(-cur_solution[0][0]-true_solution[0][0])
-            
+            regret_psrl[k,m] = abs(cur_solution[0][0]-true_solution[0][0])
+            print("PSRL cur_solution[0][0]",cur_solution[0][0], "Regret: ", regret_psrl[k,m])
             samples = np.zeros((num_actions, num_next_states))
             
             # Follow the policy to collect transition samples
@@ -178,6 +181,10 @@ if __name__ == "__main__":
                 
                 bayes_threshold = compute_bayesian_threshold(bayes_samples, nominal_point_bayes, confidence)
                 
+                #transition_prob = crobust.worstcase_l1_dst(-rewards, nominal_point_bayes, bayes_threshold)[0]
+                
+                #print("nominal_point_bayes",nominal_point_bayes,"transition_prob",transition_prob)
+                
                 for s in range(num_next_states):
                     sampled_mdp.add_transition(0, a, s+1, nominal_point_bayes[s], rewards[s])
                     
@@ -187,18 +194,18 @@ if __name__ == "__main__":
                 thresholds[2].append(bayes_threshold) # allowed deviation
             
             # Compute current solution
-            cur_solution = sampled_mdp.rsolve_mpi(b"robust_l1",np.array(thresholds))
+            cur_solution = sampled_mdp.rsolve_mpi(b"optimistic_l1",np.array(thresholds)) # solve_mpi()
             cur_policy = cur_solution.policy
             action = cur_policy[0] # action for the nonterminal state 0
-            regret_bayes_ucrl[k,m] = abs(cur_solution[0][0]-true_solution[0][0])
-            
+            regret_bayes_ucrl[k,m] = abs(abs(cur_solution[0][0])-true_solution[0][0])
+            print("Bayes UCRL cur_solution[0][0]",cur_solution[0][0], "Regret: ", regret_bayes_ucrl[k,m])
             samples = np.zeros((num_actions, num_next_states))
-            
+
             # Follow the policy to collect transition samples
             for h in range(horizon):
                 next_state = np.random.choice(num_next_states, 1, p=transitions[action])
                 samples[action, next_state] += 1
-                
+
     regret_bayes_ucrl = np.mean(regret_bayes_ucrl, axis=1)
 
     plt.plot(np.cumsum(regret_bayes_ucrl))
@@ -208,14 +215,11 @@ if __name__ == "__main__":
 ###
     # Plot regret
     plt.plot(np.cumsum(regret_psrl), label="PSRL", color='b', linestyle='--')
-    plt.plot(np.cumsum(regret_ucrl), label="UCRL", color='g', linestyle=':')
+    #plt.plot(np.cumsum(regret_ucrl), label="UCRL", color='g', linestyle=':')
     plt.plot(np.cumsum(regret_bayes_ucrl), label="Bayes UCRL", color='r', linestyle='-.')
     plt.legend(loc='best', fancybox=True, framealpha=0.3)
     plt.grid()
     plt.show()
-
-
-
 
 
 
